54a07e22b8917ec6f3b08782d4efa18622d8a090
firecracks <sir.brakatata@gmail.com>
2025-11-29 13:18:18 -0800

    Updated backend and frontend code

--- Desktop/KIRS/KIRS/src/Login.jsx
+++ Desktop/KIRS/KIRS/src/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5234/login", {
+      const response = await fetch("https://kasu-item-recovery-system.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
